bf525d68394c8f79ee23a267d64634e5f69d0fb4
philippe tarte <tarteserviceinformatique@sfr.fr>
2018-07-09 16:02:55 +0200

    first commit

---
+++ src/HelloWorld.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const HelloWorld = ({firstName}) => {
+    return <p>salut mon ami {firstName}</p>
+}
+
+export default HelloWorld
---
+++ src/StrikeWord.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+
+class StrikeWord extends Component {
+    // surcharge fonction render
+    render = () => {
+        return 
+            <div >
+                <input type='text' name="lastName" />
+            </div>
+        
+    }
+}
+
+const  tapeMot = new StrikeWord()
+
+export default tapeMot
